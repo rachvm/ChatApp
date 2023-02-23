7330bc3b844749144220c5efe3311721a8cbd13a
Rachel Morris <rachelmorris262@outlook.com>
2023-02-23 13:54:40 +0000

    sorted auth and completed delete reply

--- frontend/src/components/Auth/profile.js
+++ frontend/src/components/Auth/profile.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 
-const Profile = ( {handleName} ) => {
+const Profile = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [userMetadata, setUserMetadata] = useState(null);
 
@@ -36,17 +36,7 @@ const Profile = ( {handleName} ) => {
   
     getUserMetadata();
   }, [getAccessTokenSilently, user?.sub]);
-
-
-  useEffect((userMetadata)=>{
-  (
-    userMetadata ? (
-    handleName(userMetadata))
-  : console.log("data not returned"))
-}, [handleName])
-
-
-    
+   
   return (
     isAuthenticated && (
       <div>
